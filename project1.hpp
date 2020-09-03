bdee25cc6413f3f0b72e5c8d9162c904004eb0a5
isakaaby <70010728+isakaaby@users.noreply.github.com>
2020-09-03 22:42:04 +0200

    Update project1.hpp

--- project1.hpp
+++ project1.hpp
@@ -9,21 +9,16 @@ private:
   double *m_x;
 
 public:
-  void Initialize(double a, double b, int N, double f(double x));     //Initialize the parameters m_a, m_b  N
+  void gen_Initialize(double a, double b, int N, double f(double x));     //Initialize the parameters m_a, m_b  N
+  void spes_Initialize(double x0, double xn, int N, double f(double x));
   void set_matrix_elements(double ai, double bi, double ci);   //Setting matrix elements
-  void forward_sub();         //Forward substitution
-  void backward_sub();       //Backward substitution
-<<<<<<< HEAD
-<<<<<<< HEAD
-  void Print(double u(double x));
-=======
-  double write_to_file(double u(double x));
->>>>>>> 7400582321905d14a00ad90df346933ea44315fb
-=======
+  void gen_forward_sub();         //Forward substitution
+  void gen_backward_sub();       //Backward substitution
+  void spes_forward_sub();         //Forward substitution
+  void spes_backward_sub();       //Backward substitution
   double write_solutions_to_file(double u(double x));
   double eps(double u(double x));
   double write_eps_to_file(double *eps, int *n, int N_power);
->>>>>>> 52dbb26ff0fb93d4655f067d97199bd36bdfa353
 };
 
 #endif
